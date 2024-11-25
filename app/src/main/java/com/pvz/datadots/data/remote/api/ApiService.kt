72a910540df45eab4149b74bd17f6352a760b2e1
pziuzliaev <pziuzliaev@noveogroup.com>
2024-11-25 17:19:24 +0100

    Implement retrofit service

---
+++ app/src/main/java/com/pvz/datadots/data/remote/api/ApiService.kt
@@ -0,0 +1,17 @@
+package com.pvz.datadots.data.remote.api
+
+import com.pvz.datadots.domain.model.Point
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("api/test/points")
+    suspend fun getPoints(@Query("count") count: Int): Response<PointResponse>
+}
+
+data class PointResponse(
+    val points: List<Point>
+)
+
+
